9c13e662690e9efa0d8623924cc0e1644af0ea00
gurucse <gurunarayanancse@gmail.com>
2020-05-02 00:30:11 +0530

    Initial commit

--- tweetme/manage.py
+++ tweetme/manage.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 def main():
+
+    print("Hello world")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweetme.settings')
     try:
         from django.core.management import execute_from_command_line
