d9205381300f738d41650a69e9da8059cd64a09c
gurucse <gurunarayanancse@gmail.com>
2020-05-07 02:23:29 +0530

    Create Tweet API and Updation in list

--- tweetme/tweet/views.py
+++ tweetme/tweet/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
 import random
+from .forms import TweetForm
+
 from .models import Tweet
+
 # to render HomePage
 def home_page(request , *args , **kwargs):
     return render(request,"pages/index.html",context={},status=200)
 
-def tweet_detial_view(request , tweet_id ,*args , **kwards):
+def tweet_create_view(request, *args , **kwargs):
+
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+
+        if next_url != None:
+            return HttpResponseRedirect(next_url)
+        form = TweetForm()
+        
+    return render(request,"components/form.html",context={"form" : form},status=200)
+
+
+def tweet_detial_view(request , tweet_id ,*args , **kwards): 
     status = 200
     data = {
         "status" : status,
@@ -22,8 +40,7 @@ def tweet_detial_view(request , tweet_id ,*args , **kwards):
 
 
 def tweet_list(request,*args,**kwargs): 
-    querySet = Tweet.objects.all()
-
+    querySet = Tweet.objects.all().reverse()
     allTweets = [{"id" : item.id,"content" : item.contant,"likes" : random.randint(0,1000)} for item in querySet]
 
     data = {
